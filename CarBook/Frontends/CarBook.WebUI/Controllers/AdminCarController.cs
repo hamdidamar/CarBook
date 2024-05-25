e863bc8599f5a3bdc6b052958acc45be21693c6b
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2024-05-25 20:47:31 +0300

    admin car list

--- CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ CarBook/Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5214/api/Cars/GetAllWithIncludes");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GetAllCarWithIncludesDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }

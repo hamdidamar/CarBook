fe9cdd82568a9a0c46a7e8b0884ae3d9e015ddef
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2024-08-15 22:14:21 +0300

    Footer düzenlemeleri

--- CarBook/Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ CarBook/Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.FooterDtos;
+using CarBook.Dto.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents;
 
 public class _FooterUILayoutComponentPartial:ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("http://localhost:5214/api/Footers");
+        var responseMessageSocial = await client.GetAsync("http://localhost:5214/api/SocialMedias");
+        if (responseMessageSocial.IsSuccessStatusCode)
+        {
+            var jsonDataSocial = await responseMessageSocial.Content.ReadAsStringAsync();
+            var valuesSocial = JsonConvert.DeserializeObject<List<GetAllSocialMediaDto>>(jsonDataSocial);
+            ViewBag.socialMedia = valuesSocial;
+        }
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<GetAllFooterDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }

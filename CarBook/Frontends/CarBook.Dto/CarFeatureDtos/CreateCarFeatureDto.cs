5bd406b3bf4899872231de4093b985db8632229e
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2024-08-20 22:51:19 +0300

    Create CreateCarFeatureDto.cs

---
+++ CarBook/Frontends/CarBook.Dto/CarFeatureDtos/CreateCarFeatureDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.CarFeatureDtos;
+
+public class CreateCarFeatureDto
+{
+    public bool Available { get; set; }
+    public string CarId { get; set; }
+    public string FeatureId { get; set; }
+}
